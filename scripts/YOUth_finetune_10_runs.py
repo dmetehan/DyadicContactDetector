a7c2c4d43ab43e95a5276e3cfda6a4ec4b3178cd
M.Doyran <m.doyran@uu.nl>
2023-09-13 13:38:37 +0200

    removing unnecessary files

--- scripts/YOUth_finetune_10_runs.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-for i in range(10):
-    cmd = ['venv/bin/python', 'YOUth_train.py', 'configs/001_config.yaml', 'main', '--finetune', '--test', '--log_test_results']
-    print(cmd)
-    subprocess.Popen(cmd).wait()
